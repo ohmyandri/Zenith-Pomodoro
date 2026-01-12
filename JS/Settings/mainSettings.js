7292b30ef26b7117e353089929b8e22c1b8c63af
ohmyandri <120071660+ohmyandri@users.noreply.github.com>
2026-01-12 13:39:05 -0600

    Major advance with the settings, next step is to use a big refactor using local storage:C

---
+++ JS/Settings/mainSettings.js
@@ -0,0 +1,6 @@
+const focusSlider = document.getElementById('focusSlider');
+const restSlider = document.getElementById('restSlider');
+
+focusSlider.addEventListener('change', (e) => {
+    //We get the value:
+})
