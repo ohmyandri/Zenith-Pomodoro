fd131aea72fef3df6f65b3b225c16c638d44305f
ohmyandri <120071660+ohmyandri@users.noreply.github.com>
2026-01-13 20:46:44 -0600

    Implemented localStorage to save the sessions history to display it at history page

--- JS/localStorage/dataStorage.js
+++ JS/localStorage/dataStorage.js
@@ -3,6 +3,8 @@ const REST_KEY = "restData"
 //BEHAVIOR KEYS
 const SOUND_KEY = "soundData";
 const AUTOSTART_KEY = "autoStartData";
+//ARRAY KEY
+const SAVE_SESSION_KEY = "sessionDataArray";
 
 const DEFAULT_FOCUS = { h: 0, m: 25, s: 0 };
 const DEFAULT_REST = { h: 0, m: 5, s: 0 };
@@ -45,10 +47,20 @@ export function saveAutoStartBehavior(checkboxState){
 
 export function getSoundBehavior(){
     const checkboxSoundStateString = localStorage.getItem(SOUND_KEY);
-    return checkboxSoundStateString ? JSON.parse(checkboxSoundStateString) : false;
+    return checkboxSoundStateString ? JSON.parse(checkboxSoundStateString) : true;
 }
 
 export function getAutoStartBehavior(){
     const checkboxAutoStartStateString = localStorage.getItem(AUTOSTART_KEY);
     return checkboxAutoStartStateString ? JSON.parse(checkboxAutoStartStateString) : false;
+}
+
+export function saveSessionHistory(dataArray){
+    const arrayString = JSON.stringify(dataArray);
+    localStorage.setItem(SAVE_SESSION_KEY, arrayString)
+}
+
+export function getSessionHistory(){
+    const arrayString = localStorage.getItem(SAVE_SESSION_KEY);
+    return arrayString ? JSON.parse(arrayString) : [];
 }
